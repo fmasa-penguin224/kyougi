41d1f80c54a2e02444e82e7c942dc6f8ee4fdc6d
fmasa-penguin224 <masamoon2018@gmail.com>
2021-10-20 11:42:38 +0900

    [New}upload the file

---
+++ AtCoder_Practice/1_21.py
@@ -0,0 +1,15 @@
+import math
+
+#値をstr型で受け取る
+a, b = map(str, input().split())
+
+#文字列を合体後int型へ
+heiho = int(a+b)
+
+#値の平方数の小数点以下が0(=True)か判定
+flag = math.sqrt(heiho).is_integer()
+
+if flag:
+    print("Yes")
+else:
+    print("No")
--- AtCoder_Practice/Bishop.py
+++ AtCoder_Practice/Bishop.py
@@ -1,5 +1,17 @@
+#縦:H, 横:Wのマスを用意
 H, W = map(int, input().split())
 
-for r in range(H):
-    for c in range(W):
-        if r+c == r+c+2
+#角の置ける数
+ans=0
+
+#1*1のマスでは1つ置ける
+if H==1 or W==1:
+    ans = 1
+
+#H*Wの1/2マス置ける,奇数になるときは切り上げ
+else:
+    ans = int((H*W)/2)
+    if (H*W)%2==1:
+        ans+=1
+
+print(ans)
