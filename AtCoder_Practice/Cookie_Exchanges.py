4f998fd1175f96b96a8f18d6d75da1b0f1babe90
fmasa-penguin224 <masamoon2018@gmail.com>
2021-10-22 11:13:04 +0900

    今日の進捗

---
+++ AtCoder_Practice/Cookie_Exchanges.py
@@ -0,0 +1,31 @@
+#何回交換されるか数える関数
+def judge(A, B, C):
+    count = 0
+    #   A<B<Cだとすると最大値と最小値の差はC-A,1回目の操作後その差は(C-A)/2となる
+    #   N回目の交換でその差は(C-A)/2**Nとなるため10**9程度の数値ではlog_2(10**9)=30~40回程度の交換でiになる
+    for i in range(40):
+        # 奇数になったら終了
+        if A%2==1:
+            return i
+        elif B%2==1:
+            return i
+        elif C%2==1:
+            return i
+
+        count += 1
+
+        tmp_A = A
+        tmp_B = B
+        tmp_C = C
+
+        A = (tmp_B + tmp_C)//2
+        B = (tmp_A + tmp_C)//2
+        C = (tmp_A + tmp_B)//2
+    #40回交換しても奇数にならなければ全ての入力が同じ数である
+    return -1
+
+A, B ,C = map(int, input().split())
+
+ans = judge(A, B, C)
+
+print(ans)
