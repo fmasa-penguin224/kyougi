642290f843be1049e45850af829957e4dc70cbcc
fmasa-penguin224 <masamoon2018@gmail.com>
2021-11-07 00:03:20 +0900

    add comment

--- AtCoder_Practice/Traveling_Salesman_around_Lake.py
+++ AtCoder_Practice/Traveling_Salesman_around_Lake.py
@@ -2,10 +2,14 @@ K, N = map(int, input().split())
 
 A = list(map(int, input().split()))
 
+#湖のスタートラインを超えない場合
 ans = A[N-1] - A[0]
 
+#湖のスタートラインを超える場合
 for i in range(N):
+#選択した家から湖のスタートラインまでの距離
     select_house_to_end = K - A[i]
+#湖のスタートラインから選択された家の一つ前の家までの距離
     start_to_first_house = A[i-1]
     tmp = select_house_to_end + start_to_first_house
 
